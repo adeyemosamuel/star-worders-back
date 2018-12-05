0dab951ec2b8aabde7524020fa4679c1e0d9e7c0
Lucas Sedrez <lucassedrez@hotmail.com>
2018-12-04 22:06:13 -0200

    test(customers): removing service test

--- src/modules/customers/specs/customers.controller.spec.ts
+++ src/modules/customers/specs/customers.controller.spec.ts
@@ -1,34 +1,31 @@
 import { Test } from '@nestjs/testing';
 import { CustomersController } from '../customers.controller';
 import { CustomersService } from '../customers.service';
-import { Repository } from 'typeorm';
 import { Customer } from '../customer.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('Customers Controller Tests', () => {
 
     let customersController: CustomersController;
-    let customersService: CustomersService;
 
     beforeEach(async () => {
         const module = await Test.createTestingModule({
             controllers: [CustomersController],
             providers: [CustomersService, {
                 provide: getRepositoryToken(Customer),
-                useValue: {}
+                useValue: {
+                    find: jest.fn(() => [new Customer(), new Customer()])
+                }
             }],
         }).compile();
 
-        customersService = module.get<CustomersService>(CustomersService);
         customersController = module.get<CustomersController>(CustomersController);
     });
 
     describe('findAll', () => {
         it('should return an array of customers', async () => {
-            const result = [new Customer(), new Customer()];
-            jest.spyOn(customersService, 'findAll').mockImplementation(() => result);
 
-            expect(await customersController.findAll()).toBe(result);
+            expect(await customersController.findAll()).toEqual([new Customer(), new Customer()]);
         });
     });
 });
